dd6ba6e56ca1904183ae885ee44613a6c73afd79
Jonathan Cabrera <noj@fb.com>
2018-06-27 17:49:32 -0700

    added network

---
+++ Flixster/MoviesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesViewController.h
+//  Flixster
+//
+//  Created by Jonathan Cabrera on 6/27/18.
+//  Copyright © 2018 Jonathan Cabrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesViewController : UIViewController
+
+@end
